b8c7abd8f084fc6ed8850e21bd5f7e530abf8f40
mishokU <34416536+mishokU@users.noreply.github.com>
2019-05-20 16:33:32 +0300

    Add classes

---
+++ app/src/main/java/com/example/theflyingbird2/Bird.java
@@ -0,0 +1,35 @@
+package com.example.theflyingbird2;
+
+import android.widget.ImageView;
+
+import static com.example.theflyingbird2.MainActivity.birdSize;
+import static com.example.theflyingbird2.MainActivity.frameHeight;
+
+public class Bird extends GameObjects {
+
+    public Bird(ImageView textView, int offSetX, int offSetY) {
+        super(textView, offSetX, offSetY);
+    }
+
+    public void update(boolean flag){
+
+        jump(flag);
+
+        if (getPosY() < 0)
+            setPosY(0);
+
+        if (getPosY() > frameHeight - getImageView().getHeight())
+            setPosY(frameHeight - getImageView().getHeight());
+        getImageView().setY(getPosY());
+    }
+
+    private void jump(boolean flag) {
+        if (flag) {
+            //Touching
+            setPosY(getPosY() - 20);
+        } else {
+            //Releasing
+            setPosY(getPosY() + 20);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/theflyingbird2/GameObjects.java
@@ -0,0 +1,61 @@
+package com.example.theflyingbird2;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.example.theflyingbird2.MainActivity.birdSize;
+
+public class GameObjects {
+
+    private ImageView imageView;
+
+    private int posX;
+    private int posY;
+    private int speedX;
+    private int speedRespawnX;
+
+    public GameObjects(ImageView textView, int offSetX, int offSetY){
+        this.imageView = textView;
+    }
+
+    public int getSpeedX() {
+        return speedX;
+    }
+
+    public int getCenterX(){
+        return getPosX() + getImageView().getWidth() / 2;
+    }
+
+    public boolean intersect(int birdY){
+        return 0 <= getCenterX() && getCenterX() <= birdSize && birdY <= getCenterY() && getCenterY() <= birdY + birdSize;
+    }
+
+    public int getCenterY(){
+        return getPosY() + getImageView().getHeight() / 2;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+}
